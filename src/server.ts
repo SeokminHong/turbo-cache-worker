7bf18fc3715eb80ee23b97e54490898fe937816a
Seokmin Hong <ghdtjrald240@gmail.com>
2023-05-01 16:04:42 +0900

    Graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,25 @@ import { serve } from '@hono/node-server';
 
 import app from '.';
 
-serve({ fetch: app.fetch, port: 8787 }, ({ address, port }) => {
-  console.log(`Server listening on ${address}:${port}`);
+const server = serve(
+  { fetch: app.fetch, port: Number(process.env.PORT ?? 8787) },
+  ({ address, port }) => {
+    console.log(`Server listening on ${address}:${port}`);
+  }
+);
+
+const signals = {
+  SIGHUP: 1,
+  SIGINT: 2,
+  SIGTERM: 15,
+};
+
+Object.entries(signals).forEach(([signal, value]) => {
+  process.on(signal, () => {
+    console.log(`Received ${signal}, stopping server...`);
+    server.close(() => {
+      console.log(`server stopped by ${signal} with value ${value}`);
+      process.exit(0);
+    });
+  });
 });
